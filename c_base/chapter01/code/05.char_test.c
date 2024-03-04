1b09e155fd664f3302cda5e399d8deaa51af26b4
ShenJianPing0307 <610502705@qq.com>
2024-03-04 23:44:59 +0800

    整数类型

---
+++ c_base/chapter01/code/05.char_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    unsigned char ch;
+    ch = -1;
+    printf("ch=%d %c\n", ch, ch);
+    return 0;
+}
---
+++ c_base/chapter01/code/06.int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(int args, char *argv[]) {
+
+    printf("%d %d %d %d\n",sizeof(_Bool), sizeof(char), sizeof(short), sizeof(int));
+
+    printf("char:%d-%d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("short:%d-%d\n", SHRT_MIN, SHRT_MAX);
+    printf("int:%d-%d\n", INT_MIN, INT_MAX);
+
+    return 0;
+}
