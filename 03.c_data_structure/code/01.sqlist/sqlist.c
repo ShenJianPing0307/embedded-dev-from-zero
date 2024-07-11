effa10d4d48b427c22b14bf93119bda1e8901507
ShenJianPing0307 <610502705@qq.com>
2024-07-12 07:45:14 +0800

    顺序表的实现

--- 03.c_data_structure/code/01.sqlist/sqlist.c
+++ 03.c_data_structure/code/01.sqlist/sqlist.c
@@ -21,3 +21,78 @@ sqlink list_creat() {
     return L;
 
 }
+
+int list_clear(sqlink, L) {
+
+    if(L == NULL) {
+        return -1;
+    }
+    memset(L, 0, sizeof(sqlist));
+    L->last = -1;
+    return 0;
+
+}
+
+int list_free(sqlink L) {
+
+    if(L == NULL) {
+        return -1;
+    }
+    free(sqlink);
+    sqlink == NULL;
+    return 0;
+
+}
+/*
+* Is list empty
+*/
+int list_empty(sqlink L) {
+    if(L->last == -1) {
+        return 1;
+    }
+    return 0;
+}
+
+int list_length() {
+    if(L == NULL) {
+        return -1;
+    }
+    return (L->last+1);
+}
+
+int list_locate(sqlink L, data_t value) {
+    int i;
+    for(i = 0; i <= L->last; i++) {
+        if(L->data[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int list_insert(sqlink L, data_t value, int pos) {
+    int i;
+
+    //full
+    if(L->last == N-1) {
+        printf("list is full\n");
+        return -1;
+    }
+
+    //check param pos [0, last+1]
+    if(pos < 0 || pos > L->last+1) {
+        printf("pos is invalid\n");
+        return -1;
+    }
+
+    //move
+    for(i = L->last; i >= pos; i--) {
+        L->data[i+1] = L->data[i];
+    }
+
+    //update value last
+    L->data[pos] = value;
+    L->last++;
+
+    return 0
+}
--- 03.c_data_structure/code/01.sqlist/sqlist.h
+++ 03.c_data_structure/code/01.sqlist/sqlist.h
@@ -7,3 +7,9 @@ typedef struct {
 }sqlist, *sqlink;
 
 sqlink list_creat();
+int list_clear(sqlink L);
+int list_free(sqlist L);
+int list_empty(sqlink);
+int list_length(sqlink L);
+int list_locate(sqlink L, data_t value);
+int list_insert(sqlink L, data_t value, int pos);
