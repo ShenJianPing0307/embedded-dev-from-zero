3144f150c4f18f860948718dc42ef9061fbdf9d7
ShenJianPing0307 <610502705@qq.com>
2024-08-04 10:48:56 +0800

    链表的实现

---
+++ 03.c_data_structure/code/02.linklist/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "linklist.h"
+
+int main(int argc, const char *argv[]) {
+
+    linklist H;
+    int value;
+
+    H = list_create();
+    if (H == NULL) {
+        return -1;
+    }
+
+    printf("input:");
+    while (1) {
+        scanf("%d", &value);
+        if (value == -1) {
+            break;
+        }
+        list_tail_insert(H, value);
+        printf("input:");
+    }
+
+    list_show(H);
+    list_insert(H, 100, 0);
+    list_show(H);
+
+    return 0;
+
+}
