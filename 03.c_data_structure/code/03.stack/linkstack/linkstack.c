38edeca8d4b9384758fd293a70449a5de388064e
ShenJianPing0307 <610502705@qq.com>
2024-08-04 22:07:15 +0800

    栈的实现

---
+++ 03.c_data_structure/code/03.stack/linkstack/linkstack.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkstack.h"
+
+linkstack stack_create() {
+
+}
+
+int stack_push(linkstack s, data_t value) {
+
+}
+
+data_t stack_pop(linkstack s) {
+
+}
+
+int stack_empty(linkstack s) {
+
+}
+
+data_t stack_top(linkstack s) {
+
+}
+
+linkstack stack_free(linkstack s) {
+
+}
